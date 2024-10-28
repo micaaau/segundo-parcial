30cde854df1a18565c2984d96bf591b699ebd0ef
micaaau <ortizmicaela009@gmail.com>
2024-10-28 18:29:47 -0300

    Actualizar ej 4 parcial.py

--- ej 4 parcial.py
+++ ej 4 parcial.py
@@ -16,10 +16,10 @@ return a - b
 Si se llama a suma(2, 3), el resultado deberia ser 15, ya que el 
 decorador multiplica el resultado original 5 por 3."""
 
-def multiplicar_resultado(multiplicador):
+def multiplicar_resultado(num):
     def decorador(func):
         def envoltura(a, b):
-            return func(a, b) * multiplicador
+            return func(a, b) * num
         return envoltura
     return decorador
 
